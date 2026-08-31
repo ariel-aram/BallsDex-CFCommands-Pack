21ee34c13c49b10f7f1637e0bca580c6420ab9ed
HiboMan <hiboman@proton.me>
2026-08-31 23:58:50 +0200

    Initial commit

---
+++ cfcommands/package/__init__.py
@@ -0,0 +1,13 @@
+import logging
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ballsdex.core.bot import BallsDexBot
+
+log = logging.getLogger("ballsdex.packages.cfcommands")
+
+async def setup(bot: BallsDexBot):
+    log.info("Loading CFCommands package...")
+    from .cog import CFCommands
+    await bot.add_cog(CFCommands(bot))
+    log.info("CFCommands package loaded successfully!")
